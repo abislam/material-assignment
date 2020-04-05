17854d191269e44c289ac12e10a25039c71ebde5
Abed <aislam360@gmail.com>
2020-04-05 16:22:32 -0400

    added link to reference later

--- src/components/SimpleTextField.js
+++ src/components/SimpleTextField.js
@@ -1,3 +1,5 @@
+//https://github.com/benawad/basic-react-form/blob/3_client_side_validation/src/Form.js
+
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
@@ -17,6 +19,60 @@ export default function BasicTextFields(props) {
   return (
     <form className={classes.root} noValidate autoComplete="off">
       <TextField id="standard-basic" label={props.label} />
+    </form>
+  );
+}
+
+
+export default function ValidationTextFields() {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <div>
+        <TextField error id="standard-error" label="Error" defaultValue="Hello World" />
+        <TextField
+          error
+          id="standard-error-helper-text"
+          label="Error"
+          defaultValue="Hello World"
+          helperText="Incorrect entry."
+        />
+      </div>
+      <div>
+        <TextField
+          error
+          id="filled-error"
+          label="Error"
+          defaultValue="Hello World"
+          variant="filled"
+        />
+        <TextField
+          error
+          id="filled-error-helper-text"
+          label="Error"
+          defaultValue="Hello World"
+          helperText="Incorrect entry."
+          variant="filled"
+        />
+      </div>
+      <div>
+        <TextField
+          error
+          id="outlined-error"
+          label="Error"
+          defaultValue="Hello World"
+          variant="outlined"
+        />
+        <TextField
+          error
+          id="outlined-error-helper-text"
+          label="Error"
+          defaultValue="Hello World"
+          helperText="Incorrect entry."
+          variant="outlined"
+        />
+      </div>
     </form>
   );
 }
